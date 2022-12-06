1661f135cae9147fbaa6087804ab9229d4c32828
Ismail ‎ <ellouismail@gmail.com>
2022-12-06 21:05:03 +0100

    Structure and layout

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import {default as Stats} from './Stats';
+import {default as Navbar} from './Navbar';
+import {default as Testimonials} from './Testimonials';
+import {default as Hero} from './Hero';
+import {default as GetStarted} from './GetStarted';
+import {default as FeedbackCard} from './FeedbackCard';
+import {default as Billing} from './Billing';
+import {default as Business} from './Business';
+import {default as CardDeal} from './CardDeal';
+import {default as Clients} from './Clients';
+import {default as CTA} from './CTA';
+import {default as Footer} from './Footer';
